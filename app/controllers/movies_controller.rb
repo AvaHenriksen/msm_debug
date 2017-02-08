bbcc129e230f0f8f737b36caa286cf3ae465b2e7
AvaHenriksen <ava.henriksen@gmail.com>
2017-02-08 16:30:36 -0600

    movies section debugged

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,33 +1,41 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    render("index.html.erb")
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find(params[:id])
+    render("show.html.erb")
   end
 
   def new_form
+    render("new_form.html.erb")
   end
 
   def create_row
-    @movie.title = params[:the_title]
-    @movie.year = params[:the_year]
-    @movie.duration = params[:the_duration]
-    @movie.description = params[:the_description]
-    @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
+    movie = Movie.new
+    movie.title = params[:title]
+    movie.year = params[:year]
+    movie.duration = params[:duration]
+    movie.description = params[:description]
+    movie.image_url = params[:image_url]
+    movie.director_id = params[:director_id]
 
-    @movie.save
+    movie.save
 
-    render("show")
+    redirect_to("http://localhost:3000/movies/#{params[:id]}")
   end
 
   def edit_form
     @movie = Movie.find(params[:id])
+
+    render("edit_form.html.erb")
   end
 
   def update_row
+    @movie = Movie.find(params[:id])
+
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
@@ -35,12 +43,17 @@ class MoviesController < ApplicationController
     @movie.image_url = params[:image_url]
     @movie.director_id = params[:director_id]
 
-    render("show")
+    @movie.save
+
+    redirect_to("http://localhost:3000/movies/#{params[:id]}")
   end
 
   def destroy
-    movie = Movie.find(params[:id])
+    m = Movie.find(params[:id])
+    m.title = params[:title]
 
-    movie.destroy
+    m.destroy
+
+    redirect_to("http://localhost:3000/movies")
   end
 end
